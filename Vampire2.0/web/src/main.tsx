16c53282ceeb200139deae087208e0cc8452709a
Tyler Renelle <tylerrenelle@gmail.com>
2022-09-25 23:49:59 +0000

    it works with sst!

--- Vampire2.0/web/src/main.tsx
+++ Vampire2.0/web/src/main.tsx
@@ -1,26 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider as UrqlProvider, createClient } from "urql";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Article from "./pages/Article";
-import "./index.css";
 
-const urql = createClient({
-  url: import.meta.env.VITE_GRAPHQL_URL,
-});
+// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UrqlProvider value={urql}>
-      <App />
-    </UrqlProvider>
+    <App />
   </React.StrictMode>
 );
 
 function App() {
   return null
-  return (
+  /*return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -28,5 +20,5 @@ function App() {
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
-  );
+  );*/
 }
